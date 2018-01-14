bd24218a248a6a88657ab53350d8ee719440d361
Giedrė <32838556+giedre-stu@users.noreply.github.com>
2018-01-14 19:55:14 +0000

    Add files via upload
    - added Deck.toString() method
    - numberOfGames now increases with each game

---
+++ Deck.java
@@ -0,0 +1,138 @@
+package commandline; 
+import java.util.ArrayList;
+
+public class Deck {
+
+	/**
+	 * instance variables
+	 */
+	private ArrayList<String> categories;
+	private ArrayList<Card> cardsInDeck;
+	private int numberOfCards;
+
+	/**
+	 * constructor
+	 */
+	public Deck () 
+	{
+		cardsInDeck = new ArrayList<Card>(); // we have a new deck!
+		categories = new ArrayList<String>();
+	}
+
+
+	/**
+	 * category names are stored in an array list ('categories')
+	 * @param String c = a category name
+	 */
+
+	public void addCategory (String c)	
+	{
+		categories.add(c);
+	}
+
+
+	/**
+	 * adds a new card to the deck
+	 * @param String [] cAttributes 
+	 */
+
+	public void addCard(String [] cAttributes) 
+	{
+		cardsInDeck.add(new Card(cAttributes));
+		numberOfCards++; 
+	}
+
+
+	/**
+	 * shuffles current deck
+	 * swaps each card with another card at a random index
+	 */
+
+	public void shuffleDeck()
+	{
+		// get length of the list
+		int listLength = cardsInDeck.size();
+		Card temp; 
+
+		for (int i = 0; i < listLength; i++)
+
+			// shuffle
+
+		{
+			// generates a random integer
+			int random = (int)Math.random() * (listLength);
+
+			// swaps cards at indexes 'i' and 'random'
+			temp = cardsInDeck.get(i);
+			cardsInDeck.set(i, cardsInDeck.get(random));
+			cardsInDeck.set(random, temp);
+		}
+	}
+
+
+	/**
+	 * accessor method 
+	 * @returns category names as an array list
+	 * could also return a long String....
+	 */
+
+	private ArrayList<String> getCategories ()
+	{
+		return categories;
+	}
+	
+	
+	/**
+	 * toString
+	 * @return deck as a string 
+	 */
+	
+	public String dString () 
+	{
+		String dString = "";
+		Card temp;
+		
+		dString = dString + String.format( "%s ", cString());
+		
+		for (int i = 0; i < numberOfCards; i++)
+		{
+			temp = cardsInDeck.get(i);
+			dString = dString + String.format( "\n%s ", temp.toString());
+		}
+		
+		return dString;
+	}
+
+	
+	/**
+	 * toString method
+	 * @return categories in one String 
+	 */
+	
+	private String cString ()
+	{
+		String cString = "";
+		
+		// print categories
+		for (int i = 0; i < categories.size(); i++)
+		{
+			cString = cString + String.format( "%s ", categories.get(i));
+		}
+		
+		return cString; 	
+	}
+	
+
+	/**
+	 * accessor method
+	 * @return current deck
+	 * will need this after each shuffle!
+	 */
+	
+	public ArrayList<Card> getDeck()
+	{
+		return cardsInDeck;
+	}
+
+
+}
---
+++ Game.java
@@ -0,0 +1,30 @@
+package commandline;
+
+public class Game {
+	
+	/**
+	 *  instance variables
+	 */
+	
+	private int numberOfPlayers;
+	private Deck currentDeck; 
+	
+	
+	/**
+	 * constructor method;
+	 * every new game shuffles the deck
+	 * @param d (current deck)
+	 */
+	
+	public Game (Deck d)
+	{
+		d.shuffleDeck();
+		currentDeck = d;
+		
+		System.out.println();
+		System.out.println("Current deck printed below:");
+		System.out.println(currentDeck.dString());
+		
+	}
+
+}
