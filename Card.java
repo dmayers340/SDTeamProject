d87f72eeab1dfd75177d5e5e30a5bfff0594de8c
Giedrė <32838556+giedre-stu@users.noreply.github.com>
2018-01-19 16:30:02 +0000

    added choose number of players functionality

---
+++ Card.java
@@ -0,0 +1,70 @@
+package commandline;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Card {
+
+	/**
+	 * the only instance variable
+	 * all the attributes are stored in one array
+	 */
+
+	private String [] cardAttributes;
+
+
+	/**
+	 * constructor
+	 * @param cA = array containing attributes
+	 */
+
+	public Card(String [] cA)
+
+	{ 
+		cardAttributes = cA;
+	}
+
+
+	/**
+	 * getter method
+	 * @param index of the attribute
+	 * @return the value 
+	 */
+
+	public String getAttribute(int index) 
+
+	{
+		return cardAttributes[index];
+	}
+
+
+	/**
+	 * write to String
+	 * self explanatory
+	 */
+
+	public String toString () 
+
+	{
+		String cardString = "";
+
+		for (int i = 0; i<cardAttributes.length; i++)
+		{
+			cardString = cardString + String.format ("%s ", cardAttributes[i]); 
+		}
+
+		cardString = String.format ("%s ", cardString);
+		return cardString;
+	}
+
+	/**
+	 * return array 
+	 */
+	
+	public String [] getCard () 
+	
+	{
+		return cardAttributes;
+	}
+
+}
---
+++ Player.java
@@ -0,0 +1,114 @@
+package commandline;
+import java.util.ArrayList;
+
+public class Player { 
+
+	private String playerName;
+	private ArrayList<Card> hand;
+	private Card card;
+	private int numberOfCardsEach;
+	private boolean human; 
+	private boolean hasCards;
+
+	public Player(String pName) { 
+
+		this.playerName = pName;
+		hasCards = true;
+	}
+
+
+	public void setNumberOfCardsEach(int numC) {
+
+		this.numberOfCardsEach = numC;
+	}
+
+
+
+	public String toString() {
+
+		return playerName + hand;
+	}
+
+	public void receiveCards(ArrayList<Card>dealtCards) {
+
+		this.hand = dealtCards; 
+	}
+
+	public void receiveExtraCards(ArrayList<Card>extraCards) {
+
+		this.hand.addAll(extraCards);
+	}
+
+	public void receiveCard(Card c) 
+	
+	{
+		this.hand.add(c);
+	}
+	
+	public ArrayList<Card> getHand() {
+
+		return hand;
+
+	}
+	
+	/**
+	 * @return top card in hand
+	 */
+	
+	public Card getTopCard() 
+	
+	{
+		return hand.get(0);
+	}
+	
+	
+	/**
+	 * @return player name
+	 * need this for the interface
+	 */
+	
+	public String getName()
+	
+	{
+		return playerName;
+	}
+	
+
+	/**
+	 * set player type 
+	 */
+
+	public void setHuman ()
+
+	{
+		human = true;
+	}
+
+	/**
+	 * @return whether the user is human or not
+	 */
+
+	
+	public boolean isHuman ()
+
+	{
+		return human;
+	}
+	
+
+	/**
+	 * checks if the user has cards
+	 * @return true / false
+	 */
+	
+	
+	public boolean hasCards ()
+
+	{
+		if (hand.size()<1 || hand == null)
+			hasCards = false;
+		
+		return hasCards;
+	}
+	
+}
